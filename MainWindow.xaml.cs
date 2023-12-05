c63d68c179b741e32c5d2613b46b50bdec00b350
hyktv <hafsa.yktv@gmail.com>
2023-12-04 19:13:04 -0800

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GA_Hafsa_ListView
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+    public MainWindow()
+    {
+            InitializeComponent();
+            List<Student> students = new List<Student>();
+
+            // Create a student
+            // Assign an id number
+            // First and last name
+            Student name = new Student("Jane", "Doe");
+            students.Add(name);
+            students.Add(new Student("Will", "Cram"));
+            students.Add(new Student("Hafsa", "Mohamed"));
+            students.Add(new Student("John", "Doe"));
+            students.Add(new Student("J", "K"));
+
+
+
+            // Declare and instantiate
+
+
+            // Put on list view
+
+            //students.Add("");
+
+            lvDisplayList.ItemsSource = students;
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Hafsa_ListView
+{
+    internal class Student
+    {
+        string _firstName;
+        string _lastName;
+        string _idNumber;
+        Random rand = new Random();
+
+        // Have properties
+        public string FirstName { get => _firstName; set => _firstName = value; }
+        public string LastName { get => _lastName; set => _lastName = value; }
+        public string IdNumber { get => _idNumber; set => _idNumber = value; }
+
+        // Constructor
+        public Student(string firstName, string lastName) 
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _idNumber = rand.Next(100000000, 1000000000).ToString();
+        }
+
+
+    }
+}
